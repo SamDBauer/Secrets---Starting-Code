fde5bb45ddb986edd13f706be87086be524400af
Siming Dong <clovedsm@gmail.com>
2022-01-23 20:53:16 +1100

    fix a typo in passport.use()

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const findOrCreate = require("mongoose-findorcreate");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 const User=require("./User")
@@ -36,12 +34,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB", () => {
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(function (user, done) {
+passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  User.findById(id, function (err, user) {
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
     done(err, user);
   });
 });
@@ -52,13 +50,11 @@ passport.use(
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: "http://localhost:3000/auth/google/secrets",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, cb) {
-      console.log(profile);
 
       User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        return cb(err, user._id);
+        return cb(err, user);
       });
     }
   )
